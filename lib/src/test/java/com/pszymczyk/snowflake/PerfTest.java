d02480e471c296a90be7355a5d8fe856a1ddad72
Pawel Szymczyk <pawel.szymczyk@displate.com>
2023-03-11 21:26:44 +0100

    add perf tests

---
+++ lib/src/test/java/com/pszymczyk/snowflake/PerfTest.java
@@ -0,0 +1,44 @@
+package com.pszymczyk.snowflake;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+
+public class PerfTest {
+
+    @Test
+    void somePerfTestWithClock() {
+        var snowflake = new SnowflakeGenerator((byte) 10, Clock.systemUTC());
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000_000; i++) {
+            snowflake.randomSnowflake();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+
+    @Test
+    void somePerfTestWithSystemCurrentMilis() {
+        var snowflake = new SnowflakeGenerator((byte) 10, System::currentTimeMillis);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000_000; i++) {
+            snowflake.randomSnowflake();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+
+    @Test
+    void somePerfTestWithFixed() {
+        var snowflake = new SnowflakeGenerator((byte) 10, () -> 1678566274L);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            snowflake.randomSnowflake();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+}
